a2ccbc983181c77fd9ee0138acf564592edc48f8
Ayan George <ayan@ayan.net>
2013-09-11 22:07:57 +0000

    Minor re-formatting of common-log.l and added comments to buf.c

--- buf.c
+++ buf.c
@@ -1,40 +1,51 @@
 
 #include <assert.h>
-#include <string.h>
-#include <strings.h>
+#include <string.h>		/* for strlen() */
+#include <strings.h>	/* for strcat() */
 #include "buf.h"
 
-struct buf *
+struct buf * /* Newly allocated buf structure. */
 buf_new(void)
 {
 	struct buf *rc = malloc(sizeof (*rc));
+
+	assert(rc != NULL);
 
 	rc->data = NULL;
 	rc->nbytes = 0;
 
 	return rc;
-
 }
 
-void
+size_t /* Size of the newly allocated buffer. */
 buf_expand(struct buf *b, size_t extra)
 {
 	b->nbytes = 2 * (b->nbytes + extra);
-
 	b->data = realloc(b->data, b->nbytes);
 
 	assert(b->data != NULL);
+
+	return b->nbytes;
 }
 
-void
+/*
+ Initializes a buffer to an empty state preserving the allocated block.
+ */
+size_t /* Size of existing buffer. */
 buf_reset(struct buf *b)
 {
 	assert(b != NULL);
 
 	if (b->data != NULL && b->nbytes > 0)
 		b->data[0] = '\0';
+
+	return b->nbytes;
 }
 
+
+/*
+	If necessary, reallocates and appends a string to a buffer.
+ */
 void
 buf_strcat(struct buf *b, const char *data, size_t len)
 {
